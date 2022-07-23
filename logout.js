04239ddf38a03c8e942d9ad5c72a3da84272c6f2
PriyanshuPatil <pantuugurjar@gmail.com>
2022-07-23 18:24:29 +0530

    add

--- logout.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("continue").addEventListener("click",myfunc) ;
-function myfunc(event){
-    event.preventDefault();
-    localStorage.clear("User-data") ;
-    localStorage.clear("login-data") ;
-    alert("Logout Success-Full")
-    window.location.href="home.html"
-}
--- successfull.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function(){
-    window.location.href = "home.html";
-}, 3000);
